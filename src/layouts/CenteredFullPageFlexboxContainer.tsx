492c685571705bfb2e40be22dc4ad34670d13b9a
vpvnguyen <vpvnguyen@gmail.com>
2021-03-26 14:26:28 -0700

    defined component interface

--- src/layouts/CenteredFullPageFlexboxContainer.tsx
+++ src/layouts/CenteredFullPageFlexboxContainer.tsx
@@ -1,5 +1,13 @@
-export const CenteredFullPageFlexboxContainer = ({ children }) => (
+import { ReactNode } from "react";
+
+interface ICenteredFullPageFlexboxContainer {
+  children: ReactNode;
+}
+
+export const CenteredFullPageFlexboxContainer = (
+  props: ICenteredFullPageFlexboxContainer
+) => (
   <div className="h-screen w-full flex justify-center items-center">
-    {children}
+    {props.children}
   </div>
 );
