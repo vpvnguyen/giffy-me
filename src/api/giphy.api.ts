538d1470286f506d180276093829b8bfbc6cb802
vpvnguyen <vpvnguyen@gmail.com>
2021-03-26 16:20:17 -0700

    set giphy api url as a private static readonly constant

--- src/api/giphy.api.ts
+++ src/api/giphy.api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+// const GIPHY_URL = "https://api.giphy.com/v1/gifs";
 export class GiphyApi {
-  private static readonly GIPHY_URL = "https://api.giphy.com/v1/gifs";
+  private static readonly _url: string = "https://api.giphy.com/v1/gifs";
 
   public static searchGiphy = async (
     searchQuery: string,
@@ -10,7 +11,7 @@ export class GiphyApi {
     explicitRating: string
   ) => {
     try {
-      const url = `${GiphyApi.GIPHY_URL}/search?api_key=${process.env.REACT_APP_GIPHY_API_KEY}&q=${searchQuery}&limit=${limit}&offset=${offset}&rating=${explicitRating}&lang=en`;
+      const url = `${GiphyApi._url}/search?api_key=${process.env.REACT_APP_GIPHY_API_KEY}&q=${searchQuery}&limit=${limit}&offset=${offset}&rating=${explicitRating}&lang=en`;
 
       const response = await axios.get(url);
       console.log(response.data);
