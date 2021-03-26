e3838e0bf02a96390c798e616439310229f195f4
vpvnguyen <vpvnguyen@gmail.com>
2021-03-26 14:19:54 -0700

    import and use full page flexbox container and example login form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from "./logo.svg";
+import { CenteredFullPageFlexboxContainer } from "./layouts/CenteredFullPageFlexboxContainer";
+import { ExampleLoginForm } from "./layouts/ExampleLoginForm";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CenteredFullPageFlexboxContainer>
+      <ExampleLoginForm />
+    </CenteredFullPageFlexboxContainer>
   );
-}
+};
 
 export default App;
