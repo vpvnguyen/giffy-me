15040913bd21d26b89d258057ed9661c7e168e98
vpvnguyen <vpvnguyen@gmail.com>
2021-03-26 14:34:56 -0700

    convert App component to tyescript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { CenteredFullPageFlexboxContainer } from "./layouts/CenteredFullPageFlexboxContainer";
+import { ExampleLoginForm } from "./layouts/ExampleLoginForm";
+
+const App = () => {
+  return (
+    <CenteredFullPageFlexboxContainer>
+      <ExampleLoginForm />
+    </CenteredFullPageFlexboxContainer>
+  );
+};
+
+export default App;
