490e3c5a0d51bdfbf90682d806bc76445538156e
vpvnguyen <vpvnguyen@gmail.com>
2021-03-26 20:54:20 -0700

    get search results, pagination when searching giphy api

--- src/api/giphy.api.ts
+++ src/api/giphy.api.ts
@@ -1,24 +1,53 @@
 import axios from "axios";
 
-// const GIPHY_URL = "https://api.giphy.com/v1/gifs";
+export interface IGiphyApi {
+  searchQuery: string;
+  limit: number;
+  offset: number;
+  explicitRating: "g" | "pg" | "pg-13" | "r";
+}
+
+const getGifStillUrl = (giphyData: any, imageSizeType: any) =>
+  giphyData.images[`${imageSizeType}_still`].url;
+
+const getGifUrl = (giphyData: any, imageSizeType: any) =>
+  giphyData.images[imageSizeType].url;
+
+const imageSizeTypes = {
+  FIXED_WIDTH: `fixed_width`,
+};
+
 export class GiphyApi {
   private static readonly _url: string = "https://api.giphy.com/v1/gifs";
 
-  public static searchGiphy = async (
-    searchQuery: string,
-    limit: number,
-    offset: number,
-    explicitRating: string
-  ) => {
+  public static search = async ({
+    searchQuery,
+    limit,
+    offset,
+    explicitRating,
+  }: IGiphyApi) => {
     try {
       const url = `${GiphyApi._url}/search?api_key=${process.env.REACT_APP_GIPHY_API_KEY}&q=${searchQuery}&limit=${limit}&offset=${offset}&rating=${explicitRating}&lang=en`;
 
       const response = await axios.get(url);
-      console.log(response.data);
+      console.log(`${GiphyApi.name} searchGiphy`, response);
 
-      return response.data;
+      const searchResults = response.data.data.map(
+        (giphyData: any, index: number) => ({
+          id: giphyData.id,
+          title: giphyData.title,
+          sourceUrl: giphyData.source,
+          gifUrl: getGifUrl(giphyData, imageSizeTypes.FIXED_WIDTH),
+          stillUrl: getGifStillUrl(giphyData, imageSizeTypes.FIXED_WIDTH),
+        })
+      );
+
+      const { pagination } = response.data;
+
+      return { searchResults, pagination };
     } catch (error) {
-      console.error(error);
+      console.error(`${GiphyApi.name} Error:`, error);
+      return null;
     }
   };
 }
