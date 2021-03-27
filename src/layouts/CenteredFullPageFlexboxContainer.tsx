5725b4a142d40067299c918bb8c8cf1413c1b374
vpvnguyen <vpvnguyen@gmail.com>
2021-03-26 20:55:31 -0700

    define classes to be injected into component's classname

--- src/layouts/CenteredFullPageFlexboxContainer.tsx
+++ src/layouts/CenteredFullPageFlexboxContainer.tsx
@@ -2,12 +2,15 @@ import { ReactNode } from "react";
 
 interface ICenteredFullPageFlexboxContainer {
   children: ReactNode;
+  classes?: string;
 }
 
 export const CenteredFullPageFlexboxContainer = (
   props: ICenteredFullPageFlexboxContainer
 ) => (
-  <div className="h-screen w-full flex justify-center items-center">
+  <div
+    className={`h-screen w-full flex justify-center items-center ${props.classes}`}
+  >
     {props.children}
   </div>
 );
