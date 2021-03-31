41a1b04b78a64b69dc1ac7ab15ef4117e19541e5
vpvnguyen <vpvnguyen@gmail.com>
2021-03-31 10:16:29 -0700

    moved data fetcher hook

---
+++ src/hooks/api/useDataFetcher.tsx
@@ -0,0 +1,66 @@
+import axios from "axios";
+import { useEffect, useReducer, useState } from "react";
+
+interface IfetchStatus {
+  INIT: "FETCH_INIT";
+  SUCCESS: "FETCH_SUCCESS";
+  FAILURE: "FETCH_FAILURE";
+}
+
+const fetchStatus: IfetchStatus = {
+  INIT: "FETCH_INIT",
+  SUCCESS: "FETCH_SUCCESS",
+  FAILURE: "FETCH_FAILURE",
+};
+
+const dataFetchReducer = (state: any, action: any) => {
+  switch (action.type) {
+    case fetchStatus.INIT:
+      return { ...state, loading: true, error: false };
+    case fetchStatus.SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        error: false,
+        response: action.payload,
+      };
+    case fetchStatus.FAILURE:
+      return { ...state, loading: false, error: true };
+    default:
+      throw new Error("dataFetchReducer error: state, action");
+  }
+};
+
+export const useDataFetcher = (initialUrl: any, initialData = null) => {
+  const [url, setUrl] = useState(initialUrl);
+
+  const [state, dispatch] = useReducer(dataFetchReducer, {
+    loading: false,
+    error: false,
+    response: initialData,
+  });
+
+  useEffect(() => {
+    let runTask = true;
+
+    const fetchData = async () => {
+      dispatch({ type: fetchStatus.INIT });
+
+      try {
+        const response = await axios.get(url);
+        if (runTask) dispatch({ type: fetchStatus.SUCCESS, payload: response });
+      } catch (error) {
+        console.error("useDataFetcher Error: ", error);
+        if (runTask) dispatch({ type: fetchStatus.FAILURE });
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      runTask = false;
+    };
+  }, [url]);
+
+  return { ...state };
+};
