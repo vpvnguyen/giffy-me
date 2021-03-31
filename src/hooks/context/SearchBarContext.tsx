7fc18e6655fa9385c59c9cec168345422c8ffac5
vpvnguyen <vpvnguyen@gmail.com>
2021-03-31 13:19:25 -0700

    create search bar context to share search input and parameters across components

---
+++ src/hooks/context/SearchBarContext.tsx
@@ -0,0 +1,41 @@
+import {
+  createContext,
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useContext,
+  useState,
+} from "react";
+
+export interface ISearchBarContext {
+  searchInput: string;
+  setSearchInput: Dispatch<SetStateAction<string>>;
+}
+
+interface ISearchBarContextProviderProps {
+  children: ReactNode;
+}
+
+const SearchBarContext = createContext<ISearchBarContext>(undefined!);
+
+export const SearchBarContextProvider = ({
+  children,
+}: ISearchBarContextProviderProps) => {
+  const [searchInput, setSearchInput] = useState("");
+
+  const searchBarContextValue: ISearchBarContext = {
+    searchInput,
+    setSearchInput,
+  };
+
+  return (
+    <SearchBarContext.Provider value={searchBarContextValue}>
+      {children}
+    </SearchBarContext.Provider>
+  );
+};
+
+export const useSearchBarContext = () => {
+  const { searchInput, setSearchInput } = useContext(SearchBarContext);
+  return { searchInput, setSearchInput };
+};
