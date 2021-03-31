5bcf8cc8141fc6f587fe3e196012d0be6617b07e
vpvnguyen <vpvnguyen@gmail.com>
2021-03-31 12:49:33 -0700

    created rounded search bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { CenteredFullPageFlexboxContainer } from "./components/layouts/CenteredFullPageFlexboxContainer";
 import { GifImage } from "./components/GifImage";
-import { GiphyApiModel, IGiphyApiSearchParameters } from "./services/giphy.api";
+import {
+  GiphyApiModel,
+  IGiphyApiSearchParameters,
+  IGiphyApiSearchResponse,
+} from "./services/giphy.api";
 import { useDataFetcher } from "./hooks/api/useDataFetcher";
 
 // new component to add search parameters and construct search url
@@ -8,6 +12,23 @@ const GiphySearchBar = () => {
   return (
     <>
       <div>Search Bar | search button | parameters: limit, rating</div>
+
+      <div className="p-8">
+        <div className="bg-white flex items-center rounded-full shadow-xl">
+          <input
+            className="rounded-l-full w-full py-4 px-6 text-gray-700 leading-tight focus:outline-none"
+            id="search"
+            type="text"
+            placeholder="Search"
+          />
+
+          <div className="p-4">
+            <button className="bg-blue-500 text-white rounded-full p-2 hover:bg-blue-400 focus:outline-none w-12 h-12 flex items-center justify-center">
+              icon
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
@@ -50,22 +71,24 @@ const ViewGiphyList = () => {
       {error && <div>There was an error loading the user list.</div>}
 
       {response &&
-        response.data.data.map((value: any, index: number) => (
-          <div key={index} className="p-4">
-            <div>ID: {value.id}</div>
-            <div>Title: {value.title}</div>
-            {value.source && (
-              <a href={value.source} target="_blank" rel="noreferrer">
-                Source
-              </a>
-            )}
-            <GifImage
-              title={value.title}
-              gifUrl={value.images.fixed_width.url}
-              stillUrl={value.images.fixed_width_still.url}
-            />
-          </div>
-        ))}
+        response.data.data.map(
+          (value: IGiphyApiSearchResponse, index: number) => (
+            <div key={index} className="p-4">
+              <div>ID: {value.id}</div>
+              <div>Title: {value.title}</div>
+              {value.source && (
+                <a href={value.source} target="_blank" rel="noreferrer">
+                  Source
+                </a>
+              )}
+              <GifImage
+                title={value.title}
+                gifUrl={value.images.fixed_width.url}
+                stillUrl={value.images.fixed_width_still.url}
+              />
+            </div>
+          )
+        )}
     </div>
   );
 };
