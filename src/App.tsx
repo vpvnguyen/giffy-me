56990a85d7e6cfe3cb1f01a099d518062d7f9a47
vpvnguyen <vpvnguyen@gmail.com>
2021-04-05 15:45:40 -0700

    nest search context and handlers within search bar component; set padding to select option to push dropdown icon right

--- src/App.tsx
+++ src/App.tsx
@@ -16,88 +16,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch, faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import { faTimesCircle } from "@fortawesome/free-regular-svg-icons";
 
-const SearchBar = (props: any) => (
-  <form onSubmit={props.handleClickSearchButton}>
-    <div className="flex items-center rounded-full border border-gray-400 hover:border-gray-600">
-      <input
-        className="w-full rounded-full py-4 px-6 text-gray-700 focus:outline-none appearance-none"
-        id="search"
-        type="text"
-        placeholder={props.searchQueryHistory || "Search Gifs"}
-        name={`searchQuery`}
-        value={props.searchQuery}
-        onChange={props.handleChangeSearchInput}
-        onKeyPress={props.handleKeyPress}
-      />
-
-      <div className="p-4">
-        {props.searchQuery && (
-          <button
-            type="reset"
-            onClick={props.handleClickClearSearch}
-            className="text-gray-400 hover:text-red-400"
-          >
-            <FontAwesomeIcon icon={faTimesCircle} />
-          </button>
-        )}
-      </div>
-
-      <div className="h-4 border border-r border-gray-300" />
-
-      <SelectDropdown
-        name={`explicitRating`}
-        handleChangeSearchInput={props.handleChangeSearchInput}
-      />
-
-      <div className="h-4 border border-r border-gray-300" />
-
-      <div className="p-4">
-        <button type="submit" className="text-blue-400 hover:text-blue-600">
-          <FontAwesomeIcon icon={faSearch} />
-        </button>
-      </div>
-    </div>
-  </form>
-);
-
-const SelectDropdown = (props: any) => {
-  const explicitRatingSelectOptions = [
-    { key: "g", name: "G" },
-    { key: "pg", name: "PG" },
-    { key: "pg-13", name: "PG-13" },
-    { key: "r", name: "R" },
-  ];
-
-  return (
-    <>
-      <div className="flex items-center px-4 text-xs text-gray-400 hover:text-blue-600">
-        <select
-          className="bg-white hover:text-blue-600 focus:outline-none cursor-pointer appearance-none"
-          name={props.name}
-          onChange={props.handleChangeSearchInput}
-        >
-          {explicitRatingSelectOptions.map((value: any) => (
-            <option value={value.key} key={value.key}>
-              Rating: {value.name}
-            </option>
-          ))}
-        </select>
-
-        <FontAwesomeIcon icon={faChevronDown} />
-      </div>
-    </>
-  );
-};
-
-const Header = () => {
-  console.log("Header()");
-
+const SearchBar = () => {
   const { searchInput, setSearchInput } = useSearchBarContext();
   const { setSearchUrl } = useSearchUrlContext();
   const { searchHistory, setSearchHistory } = useSearchHistoryContext();
 
   const handleChangeSearchInput = (event: any) => {
-    console.log("Header handleChangeSearchInput", event.target.value);
+    console.log("SearchBar handleChangeSearchInput", event.target.value);
 
     setSearchInput({
       ...searchInput,
@@ -108,11 +33,11 @@ const Header = () => {
   const handleClickSearchButton = (event: any) => {
     event.preventDefault();
 
-    console.log("handleClickSearchButton searchInput", searchInput);
+    console.log("SearchBar handleClickSearchButton searchInput", searchInput);
 
     const url: string = GiphyApiModel.getSearchUrl(searchInput);
 
-    console.log("handleClickSearchButton url", url);
+    console.log("SearchBar handleClickSearchButton url", url);
     setSearchUrl(url);
 
     setSearchInput({ ...searchInput, searchQuery: "" });
@@ -135,15 +60,85 @@ const Header = () => {
   };
 
   return (
+    <form onSubmit={handleClickSearchButton}>
+      <div className="flex items-center rounded-full border border-gray-400 hover:border-gray-600">
+        <input
+          className="w-full rounded-full py-4 px-6 text-gray-700 focus:outline-none appearance-none"
+          id="search"
+          type="text"
+          placeholder={searchHistory.searchQuery || "Search Gifs"}
+          name={`searchQuery`}
+          value={searchInput.searchQuery}
+          onChange={handleChangeSearchInput}
+          onKeyPress={handleKeyPress}
+        />
+
+        <div className="p-4">
+          {searchInput.searchQuery && (
+            <button
+              type="reset"
+              onClick={handleClickClearSearch}
+              className="text-gray-400 hover:text-red-400"
+            >
+              <FontAwesomeIcon icon={faTimesCircle} />
+            </button>
+          )}
+        </div>
+
+        <div className="h-4 border border-r border-gray-300" />
+
+        <SelectDropdown
+          name={`explicitRating`}
+          handleChangeSearchInput={handleChangeSearchInput}
+        />
+
+        <div className="h-4 border border-r border-gray-300" />
+
+        <div className="p-4">
+          <button type="submit" className="text-blue-400 hover:text-blue-600">
+            <FontAwesomeIcon icon={faSearch} />
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+const SelectDropdown = (props: any) => {
+  const explicitRatingSelectOptions = [
+    { key: "g", name: "G" },
+    { key: "pg", name: "PG" },
+    { key: "pg-13", name: "PG-13" },
+    { key: "r", name: "R" },
+  ];
+
+  return (
+    <>
+      <div className="flex items-center px-2 text-xs text-gray-400 hover:text-blue-600">
+        <select
+          className="pr-1 bg-white hover:text-blue-600 focus:outline-none cursor-pointer appearance-none"
+          name={props.name}
+          onChange={props.handleChangeSearchInput}
+        >
+          {explicitRatingSelectOptions.map((value: any) => (
+            <option value={value.key} key={value.key}>
+              Rating: {value.name}
+            </option>
+          ))}
+        </select>
+
+        <FontAwesomeIcon icon={faChevronDown} />
+      </div>
+    </>
+  );
+};
+
+const Header = () => {
+  console.log("Header()");
+
+  return (
     <div className="w-full border border-gray-400 py-12">
-      <SearchBar
-        handleChangeSearchInput={handleChangeSearchInput}
-        handleClickSearchButton={handleClickSearchButton}
-        handleClickClearSearch={handleClickClearSearch}
-        handleKeyPress={handleKeyPress}
-        searchQueryHistory={searchHistory.searchQuery}
-        searchQuery={searchInput.searchQuery}
-      />
+      <SearchBar />
     </div>
   );
 };
