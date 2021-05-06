840a8be2d9bd3239071041a15e12483c582626fc
Vincent Nguyen <vnguyen@jnrcorp.com>
2021-05-06 11:09:54 -0700

    log error when initializing

--- src/services/giphy.api.ts
+++ src/services/giphy.api.ts
@@ -12,14 +12,27 @@ export interface IGiphyApiSearchResponse {
   images: any;
 }
 export class GiphyApiModel {
-  private static readonly _baseUrl: string = "https://api.giphy.com/v1/gifs";
-  private static readonly _apiKey: string =
+  private readonly _baseUrl: string = "https://api.giphy.com/v1/gifs";
+  private readonly _apiKey: string =
     process.env.REACT_APP_GIPHY_API_KEY || "";
 
-  public static getSearchUrl = (
+    constructor () {
+      try {
+        this.init()
+      } catch (error) {
+        console.error(`${GiphyApiModel.name}`, error)
+      }
+    }
+
+  public getSearchUrl = (
     searchParameters: IGiphyApiSearchParameters
   ): string => {
     const { searchQuery, limit, offset, explicitRating } = searchParameters;
-    return `${GiphyApiModel._baseUrl}/search?api_key=${GiphyApiModel._apiKey}&q=${searchQuery}&limit=${limit}&offset=${offset}&rating=${explicitRating}&lang=en`;
+    return `${this._baseUrl}/search?api_key=${this._apiKey}&q=${searchQuery}&limit=${limit}&offset=${offset}&rating=${explicitRating}&lang=en`;
   };
+
+  private init = () => {
+    if (!process.env.REACT_APP_GIPHY_API_KEY) throw new Error(`API key does not exist in ${GiphyApiModel.name}. API Key: ${process.env.REACT_APP_GIPHY_API_KEY}. Visit: https://developers.giphy.com/docs/api#quick-start-guide for an API Key`)
+  }
+
 }
